7999c6a37b6b73df66e40a5a608260771a014f11
pcmoraesmenezes <paulo.moraes@sou.unifal-mg.edu.br>
2024-10-12 18:21:41 -0300

    :sparkles: feat: Using node to run a intermediate connection between flask and the app

---
+++ node-intermediary/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors({ credentials: true, origin: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+let sessionCookies = '';
+
+function storeSessionCookies(response) {
+    const cookies = response.headers['set-cookie'];
+    if (cookies) {
+        sessionCookies = cookies.join('; ');
+    }
+}
+
+app.post('/train', async (req, res) => {
+    try {
+        const response = await axios.post('http://127.0.0.1:5000/train', req.body, {
+            withCredentials: true,
+            headers: { Cookie: sessionCookies },
+        });
+        storeSessionCookies(response);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.post('/predict', async (req, res) => {
+    try {
+        const response = await axios.post('http://127.0.0.1:5000/predict', req.body, {
+            withCredentials: true,
+            headers: { Cookie: sessionCookies },
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/coefficients', async (req, res) => {
+    try {
+        const response = await axios.get('http://127.0.0.1:5000/coefficients', {
+            withCredentials: true,
+            headers: { Cookie: sessionCookies },
+        });
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Node.js server running on http://localhost:${PORT}`);
+});
